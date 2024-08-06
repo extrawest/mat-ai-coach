a599d0691d49c7e4db0393804ef21c7cf212189c
Vitalii Karietin <vitalii.karietin@extrawest.com>
2024-08-06 16:50:34 +0300

    Initial commit. App layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+	return (
+		<div className="p-4 flex justify-between items-center bg-black text-white">
+			{/* TODO: Logo */}
+			<Link href="/">
+			</Link>
+			{/* TODO: routes */}
+			{/* TODO: user button */}
+		</div>
+	)
+}
+
+export default Navbar
